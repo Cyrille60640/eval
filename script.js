213922ed840a3398f502c57b01481761277c70f6
Cyrille60640 <cyrillebigorgne@hotmail.fr>
2022-12-23 15:10:51 +0100

    dev js Class player

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
+//globle variable
 let dice = document.getElementById('dice')
+let player_select = 1
+
+//class player
+class player{
+    constructor(round,global){
+        this.round=round
+        this.global=global
+
+        }
+        InitGlobal = ()=>{
+            this.round=0
+            this.global=0
+        }
+
+        InitRound = ()=>{
+            this.round=0
+            
+        }
+
+        AddRound= (value_dice)=>{
+            this.round += value_dice
+           // console.log(this.round)
+        }
+
+
+        AddGlobal = ()=>{
+            this.global += this.round
+        }
+        
+        DisplayRound=()=>{
+            console.log(this.round)
+        }
+    
+}
+
+//init class player for 2 players
+let player1 = new player(0,0)
+let player2 = new player(0,0)
+
 
 dice.addEventListener('click',()=>{
 
@@ -8,4 +48,40 @@ dice.addEventListener('click',()=>{
     //displays the image corresponding to the calculated value
     dice.src = 'image/dice-' + dice_value + '.png';
 
-})
+
+    switch(player_select){
+        case 1:
+            console.log("player1")
+            if (dice_value==1){
+                console.log("init")
+                player1.InitRound()
+                player_select=2
+            }else{
+                console.log("add")
+                player1.AddRound(dice_value)
+                player1.DisplayRound()
+                player2.DisplayRound()
+            }
+            break
+
+        case 2:
+            console.log("player2")
+            if (dice_value==1){
+                console.log("init")
+                player2.InitRound()
+                player_select=1
+            }else{
+                console.log("add")
+                player2.AddRound(dice_value)
+                player1.DisplayRound()
+                player2.DisplayRound()
+            }
+            break
+    }
+
+
+    
+
+    }
+
+)
